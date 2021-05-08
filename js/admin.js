47a26ddab363351e51727f8e7b84bcc0ab412a68
Luca Giannoccaro <giannoccaro.luca@volta.ts.it>
2021-05-08 12:46:20 +0200

    Signed-off-by: Luca Giannoccaro <giannoccaro.luca@volta.ts.it>

--- js/admin.js
+++ js/admin.js
@@ -1,81 +1,75 @@
+function richiestaImmagini(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', 'php/sliderImages.php?', true);
+    xhr.send();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState==4 && xhr.status==200){
+            var resp = xhr.responseText;
+            window.localStorage.clear();
+            window.localStorage.setItem('listaImmagini', resp);
+            return immagini = JSON.parse(resp);  
 
+        }
+    } 
+}
+
+//create all the item tha are in the local storage
+var  listaElementi=JSON.parse(window.localStorage.getItem('listaImmagini'));
+var container = document.querySelector('#data-container');
+for(let i=0; i<listaElementi.length; i++){
+    let item = document.createElement('div');
+    item.classList.add('item');
+    item.id = listaElementi[i][0];
+
+    let img = document.createElement('img');
+    img.src=listaElementi[i][2];
+
+    let didivTesto = document.createElement('div');
+    didivTesto.classList.add('testo');
+
+
+    let nome = document.createElement('p');
+    let testoNome = document.createTextNode(listaElementi[i][1]);
+    nome.appendChild(testoNome);
+    didivTesto.appendChild(nome);
+
+    let elimina = document.createElement('p');
+    elimina.classList.add('elimina');
+    let testoElimina = document.createTextNode('Elimina');
+    elimina.appendChild(testoElimina);
+    elimina.addEventListener('click', (evt) => {
+        rimuoviImmagine(listaElementi[i][0], item);
+    })
+    didivTesto.appendChild(elimina);
     
-    function richiestaImmagini(){
-        var xhr = new XMLHttpRequest();
-        xhr.open('post', 'php/sliderImages.php?', true);
-        xhr.send();
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState==4 && xhr.status==200){
-                var resp = xhr.responseText;
-                window.localStorage.clear();
-                window.localStorage.setItem('listaImmagini', resp);
-                return immagini = JSON.parse(resp);  
 
-            }
-        } 
-    }
-   
-   
-   var  listaElementi=JSON.parse(window.localStorage.getItem('listaImmagini'));
-    var container = document.querySelector('#data-container');
-    for(let i=0; i<listaElementi.length; i++){
-        let item = document.createElement('div');
-        item.classList.add('item');
-        item.id = listaElementi[i][0];
+    let divFrecce = document.createElement('div');
+    divFrecce.classList.add('freccie')
 
-        let img = document.createElement('img');
-        img.src=listaElementi[i][2];
+    let frecciaSinistra = document.createElement('p');
+    let testoFrecciaSinistra = document.createTextNode('<');
+    frecciaSinistra.addEventListener('click', (evt) => {
+        spostaSu(listaElementi[i][0], item);
+    })
 
-        let didivTesto = document.createElement('div');
-        didivTesto.classList.add('testo');
+    let frecciaDestra = document.createElement('p');
+    let testoFrecciaDestra = document.createTextNode('>');
+    frecciaDestra.addEventListener('click', (evt) => {
+        spostaGiu(listaElementi[i][0], item);
+    })
 
+    frecciaSinistra.appendChild(testoFrecciaSinistra);
+    frecciaDestra.appendChild(testoFrecciaDestra);
+    divFrecce.appendChild(frecciaSinistra);
+    divFrecce.appendChild(frecciaDestra);
 
-        let nome = document.createElement('p');
-        let testoNome = document.createTextNode(listaElementi[i][1]);
-        nome.appendChild(testoNome);
-        didivTesto.appendChild(nome);
+    item.appendChild(img)
+    item.appendChild(didivTesto);
+    item.appendChild(divFrecce);
+    container.appendChild(item);
+}
 
-        let elimina = document.createElement('p');
-        elimina.classList.add('elimina');
-        let testoElimina = document.createTextNode('Elimina');
-        elimina.appendChild(testoElimina);
-        elimina.addEventListener('click', (evt) => {
-            rimuoviImmagine(listaElementi[i][0], item);
-        })
-        didivTesto.appendChild(elimina);
-        
-
-        let divFrecce = document.createElement('div');
-        divFrecce.classList.add('freccie')
-
-
-        let frecciaSinistra = document.createElement('p');
-        let testoFrecciaSinistra = document.createTextNode('<');
-        frecciaSinistra.addEventListener('click', (evt) => {
-            spostaSu(listaElementi[i][0], item);
-        })
-
-        let frecciaDestra = document.createElement('p');
-        let testoFrecciaDestra = document.createTextNode('>');
-        frecciaDestra.addEventListener('click', (evt) => {
-            spostaGiu(listaElementi[i][0], item);
-        })
-
-        frecciaSinistra.appendChild(testoFrecciaSinistra);
-        frecciaDestra.appendChild(testoFrecciaDestra);
-        divFrecce.appendChild(frecciaSinistra);
-        divFrecce.appendChild(frecciaDestra);
-
-
-
-        item.appendChild(img)
-        item.appendChild(didivTesto);
-        item.appendChild(divFrecce);
-        container.appendChild(item);
-
-    }
-
-
+//remove one item. local + database (ajax)
 function rimuoviImmagine(id, item){
     var xhr = new XMLHttpRequest();
     xhr.open('get', 'php/eliminaImmagine.php?idItem='+id, true);
@@ -89,6 +83,7 @@ function rimuoviImmagine(id, item){
     window.localStorage.setItem('immagini', JSON.stringify(listaElementi));
 }
 
+//move up one item. local + database (ajax)
 function spostaSu(id, item){
     for(let i=0; i<listaElementi.length; i++){
         if(listaElementi[i][0]==id){
@@ -108,6 +103,7 @@ function spostaSu(id, item){
     }
 }
 
+//move down one item. local + database (ajax)
 function spostaGiu(id, item){
     for(let i=0; i<listaElementi.length; i++){
         if(listaElementi[i][0]==id){
@@ -124,13 +120,6 @@ function spostaGiu(id, item){
         }
     }
 }
-
-// var submit = document.querySelector('#submit');
-// submit.addEventListener('click', ()=>{
-//     window.localStorage.clear();
-//     window.localStorage.setItem('listaImmagini', richiestaImmagini());
-//     location.reload();
-// })
 
 
 
